b9c92fa342d10f31ef49145578ebc3be46a8b887
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-22 13:23:25 -0400

    ship can be moved with left and right arrow keys

--- script.js
+++ script.js
@@ -25,8 +25,28 @@ function create() {
   //set player to game.add.sprite to enable body physics
   player = game.add.sprite(game.canvas.width / 2, game.canvas.height - 100, 'playerShip');
   game.physics.arcade.enable(player, Phaser.Physics.ARCADE); //set player physics
+  player.body.collideWorldBounds = true; //player cannot leave world bounds
   console.log(2);
-  player.body.velocity.set(150, 150);
+  player.anchor.set(0.5, 1); //position players anchor point to the middle of sprite
+  player.body.immovable = true; //player wont move upon collision
+  player.x = game.input.x || game.world.width * 0.5;
+
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+  player.body.velocity.x = 0;
+
+  if (cursors.left.isDown) {
+    player.body.velocity.x = -200;
+    player.animations.play('left');
+  } else if (cursors.right.isDown) {
+    player.body.velocity.x = 200;
+    player.animations.play('right');
+  } else {
+    player.animations.stop();
+  }
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.body.velocity.y = -400;
+  }
+}
