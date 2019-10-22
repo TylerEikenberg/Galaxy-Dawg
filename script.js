02ace7b9df0001d161807149a06787d8f194a5af
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-22 16:41:18 -0400

    enemies move back and forth

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ let lasers;
 let laserTime = 0;
 let enemies = []; //to hold all the enemies
 
-//preload function preloads all the games assets
+/******************
+ *
+ * preload function preloads all the games assets
+ * */
 function preload() {
   let mouseClick;
   let leftKey;
@@ -45,10 +48,10 @@ function preload() {
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE); //add physics engine
-  game.input.mouse.capture = true;
+  //   game.input.mouse.capture = true; ADD CLICK FUNCTIONALITY LATER *********
 
   //set keys to keyboard input
-  game.mouseClick = game.input.pointer1;
+  //   game.mouseClick = game.input.pointer1;
   game.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
   game.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
   game.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -59,19 +62,19 @@ function create() {
   game.physics.arcade.enable(player, Phaser.Physics.ARCADE); //set player physics
   player.body.collideWorldBounds = true; //player cannot leave world bounds
   console.log(2);
-  player.anchor.set(0.5, 1); //position players anchor point to the middle of sprite
+  player.anchor.set(0.5, 1); //position player's anchor point to the middle of sprite
   player.body.immovable = true; //player wont move upon collision
   player.x = game.input.x || game.world.width * 0.5;
-  player.body.velocity.x = 200; //set default x velocity to 150 so ship moves on it own
+  player.body.velocity.x = 200; //set default x velocity to 200 so ship moves on it own
 
   /**
    * create player laser bullet
    */
 
-  lasers = game.add.group();
+  lasers = game.add.group(); //create group of lasers
   lasers.enableBody = true;
   game.physics.arcade.enable(lasers, Phaser.Physics.ARCADE);
-  lasers.createMultiple(100, 'laser');
+  lasers.createMultiple(100, 'laser'); //add 100 laser bullets to group
   lasers.setAll('anchor.x', 0.5);
   lasers.setAll('anchor.y', 0.5);
   lasers.setAll('scale.x', 0.5);
@@ -91,10 +94,11 @@ function create() {
 
   //for loop to create enemies and add them to enemies array
   for (let i = 0; i <= 2; i++) {
-    enemy = game.add.sprite(game.canvas.width - 300 * i, game.canvas.height - 600, 'enemyShip');
+    enemy = game.add.sprite(game.canvas.width - 200 * i, game.canvas.height - 600, 'enemyShip');
     game.physics.arcade.enable(enemy, Phaser.Physics.ARCADE);
     enemy.body.collideWorldBounds = true;
-    enemy.body.velocity.x = 100;
+    enemy.body.velocity.x = 250;
+    enemy.body.bounce.setTo(1, 1);
     enemies[i] = enemy;
     console.log;
   }
@@ -115,7 +119,6 @@ function create() {
  *
  *  update function holds code on updating game objects
  * */
-let playerVelocity = 200;
 function update() {
   /**
    * COME BACK TO THIS LATER
@@ -151,7 +154,7 @@ function update() {
   game.physics.arcade.collide(lasers, enemies, destroyEnemy);
 }
 
-/**
+/*********************
  * function fireLaser
  * first if statement checks if previous laser fired time has elapsed enough
  * laser is set to first laser in lasers group
@@ -171,6 +174,11 @@ function fireLaser() {
   }
 }
 
+/******************
+ *
+ * function destroyEnemy kills the laser and enemy upon
+ * collision between the two
+ */
 function destroyEnemy(enemy, laser) {
   enemy.kill();
   laser.kill();
