c354485dfd850b182c741c2ee8eb20478b39bee3
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-22 12:12:29 -0400

    player ship appears on screen in gamespace

--- script.js
+++ script.js
@@ -12,10 +12,20 @@ const game = new Phaser.Game(400, 730, Phaser.AUTO, 'game-wrapper', {
 let player;
 
 //preload function preloads all the games assets
-preload = () => {
+function preload() {
   game.load.image('playerShip', 'assets/spaceship.png');
+  console.log(1);
   // game.load.spritesheet('woof', 'images/woof.png', 32, 32);
-};
+}
 
 //function create holds all the game logic
-create = () => {};
+function create() {
+  //not sure what kind of physics the game will have yet
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  //set player to playerShip
+  player = game.add.image(game.canvas.width / 2, game.canvas.height - 100, 'playerShip');
+  console.log(player.body);
+  //   player.body.collideWorldBounds = true;
+}
+
+function update() {}
