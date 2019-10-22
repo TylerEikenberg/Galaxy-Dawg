3b7a75e40e1e605bb5969d26aff3a571bd082b89
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-22 13:53:55 -0400

    creating enemies

--- script.js
+++ script.js
@@ -10,17 +10,24 @@ const game = new Phaser.Game(400, 730, Phaser.AUTO, 'game-wrapper', {
 });
 
 let player;
+let enemies = [];
 
 //preload function preloads all the games assets
 function preload() {
+  //player ship
   game.load.image('playerShip', 'assets/spaceship.png');
+  //enemy ship
+  game.load.image('enemyShip', 'assets/enemy.png');
   console.log(1);
-  // game.load.spritesheet('woof', 'images/woof.png', 32, 32);
 }
 
-//function create holds all the game logic
+/*****************************************
+ *  function create holds all the game logic
+ * */
+
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE); //add physics engine
+
   //set player to playerShip
   //set player to game.add.sprite to enable body physics
   player = game.add.sprite(game.canvas.width / 2, game.canvas.height - 100, 'playerShip');
@@ -30,23 +37,47 @@ function create() {
   player.anchor.set(0.5, 1); //position players anchor point to the middle of sprite
   player.body.immovable = true; //player wont move upon collision
   player.x = game.input.x || game.world.width * 0.5;
-  player.body.velocity.x = 150; //set default x velocity to 150 so ship moves on it own
+  player.body.velocity.x = 200; //set default x velocity to 150 so ship moves on it own
 
+  //set variable cursors to keyboard input
   cursors = game.input.keyboard.createCursorKeys();
+
+  /**
+   * Create enemies
+   */
+  //create 6 enemies
+  //add enemy to game
+  //make enemy move side to side
+  //give enemy collision
+  //make enemy shoot bullet
+  //make enemy blow up ONLY WHEN hit by player bullet
+
+  //for loop to create enemies and add them to enemies array
+  for (let i = 0; i <= 2; i++) {
+    enemy = game.add.sprite(game.canvas.width - 300 * i, game.canvas.height - 600, 'enemyShip');
+    game.physics.arcade.enable(enemy, Phaser.Physics.ARCADE);
+    enemy.body.collideWorldBounds = true;
+    enemies[i] = enemy;
+    console.log;
+  }
+
+  //   enemy = game.add.sprite(game.canvas.width / 2, game.canvas.height - 600, 'enemyShip');
+  //   game.physics.arcade.enable(enemy, Phaser.Physics.ARCADE);
+  //   enemy.body.collideWorldBounds = true;
 }
 
-//update function holds code on updating game objects
+/*****************************************
+ *  update function holds code on updating game objects
+ * */
+
 function update() {
   if (cursors.left.isDown) {
     player.body.velocity.x = -200;
-    player.animations.play('left');
+    // player.animations.play('left');
   } else if (cursors.right.isDown) {
     player.body.velocity.x = 200;
-    player.animations.play('right');
+    // player.animations.play('right');
   } else {
-    player.animations.stop();
-  }
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.body.velocity.y = -400;
+    // player.animations.stop();
   }
 }
